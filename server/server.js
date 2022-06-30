79f15337c18682880ca8cd7db939dc1eac35f7c1
DreamIce-1 <christiangazzingan38@gmail.com>
2022-06-30 11:00:21 +0800

    fixing deployoment

--- server/server.js
+++ server/server.js
@@ -16,11 +16,13 @@ app.use('/api/message', messageRouter)
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, '../build')))
+
+  console.log(path.resolve(__dirname, '../', 'build', 'index.html'))
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'),
+      path.resolve(__dirname, '../', 'build', 'index.html'),
     ),
   )
 } else {
